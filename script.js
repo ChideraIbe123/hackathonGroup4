06f20d682bf0ee7033b33d5fb2c5b0ae253edf4f
Sami S <sshalabi24@nmhschool.org>
2022-07-15 22:18:31 +0800

    hide and show buttons

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log(hello)
+var bt2 = document.getElementById("button2")
+
+function showbutton(){
+    console.log("hi");
+    if(bt2.style.display === "none"){
+        bt2.style.display = "block";
+    } else{
+        bt2.style.display = "none";
+    }
+}
