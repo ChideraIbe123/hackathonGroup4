7a8ab7acaf8206722033f9aaf195a142bf65e0e4
Chidera Ibe <chideribe482005@gmail.com>
2022-07-18 08:23:19 -0500

    adding nothing

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log(hello)
+let BtnEl = document.getElementById("myBtn-1");
+
+BtnEl.addEventListener("click", function() {
+    console.log("Button Clicked");
+    BtnEl.classList.remove("hidden");
+})
+
